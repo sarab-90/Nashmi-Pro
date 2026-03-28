692697f53dfe73ed83236e7a36ce0594884304ef
sarab-90 <sarabakash.12345@gmail.com>
2026-03-28 16:07:26 +0300

    Refactor code structure for improved readability and maintainability

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from "express";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import cors from "cors";
-// import helmet from "helmet";
+import helmet from "helmet";
 import connectDB from "./src/config/db.js";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
